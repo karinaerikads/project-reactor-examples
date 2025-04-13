029acd98920cc1a11a72e0aecd55b83a1bef9cc9
karinaerikads <karinaerikads48@gmail.com>
2025-04-13 18:15:45 -0300

    adiciona model Video

---
+++ project-reactor-examples/src/main/java/com/reactor/project_reactor_examples/Video.java
@@ -0,0 +1,47 @@
+package com.reactor.project_reactor_examples;
+
+public class Video {
+    private String name;
+    private String description;
+    private Integer likes;
+    private Integer views;
+
+    public Video(String name, String description, Integer likes, Integer views) {
+        this.name = name;
+        this.description = description;
+        this.likes = likes;
+        this.views = views;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+}
