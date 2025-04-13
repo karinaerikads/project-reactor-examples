6294606bfecf524407eb0a64a8dcf21b35d8696b
karinaerikads <karinaerikads48@gmail.com>
2025-04-13 18:06:13 -0300

    primeiro commit projeto reativo

---
+++ project-reactor-examples/src/test/java/com/reactor/project_reactor_examples/ProjectReactorExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactor.project_reactor_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
